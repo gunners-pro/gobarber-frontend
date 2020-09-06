fbde3a419c6e40f15ae856ea69f05d080efe45d4
Fabricyo Barreto <bass.barreto@gmail.com>
2020-09-06 16:49:29 -0300

    The application was globally styled.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
       <h1>Olá mundo</h1>
+      <GlobalStyles />
     </>
   );
 };
