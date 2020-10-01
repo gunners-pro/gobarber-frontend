428f66274c4eec10aa3899dc1fb03e9835b49ee3
Fabricyo Barreto <bass.barreto@gmail.com>
2020-10-01 20:42:01 -0300

    the validation functionality of registration with the yup library was made.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import LogoImg from '../../assets/logo.svg';
 
@@ -9,9 +10,21 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object) {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
