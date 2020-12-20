b2bd415fe805d732e04671e023c88346c21228a5
Fabricyo Barreto <bass.barreto@gmail.com>
2020-12-20 18:34:53 -0300

    The API call was made by sending the token and the new password. the password reset part has been completed.

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,9 +1,10 @@
 import React, { useRef, useCallback } from 'react';
 import { FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import LogoImg from '../../assets/logo.svg';
 
@@ -12,6 +13,7 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { useToast } from '../../hooks/toast';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -21,6 +23,7 @@ interface ResetPasswordFormData {
 const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const history = useHistory();
+  const location = useLocation();
 
   const { addToast } = useToast();
 
@@ -38,6 +41,19 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, { abortEarly: false });
 
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
         history.push('/');
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
@@ -53,7 +69,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
